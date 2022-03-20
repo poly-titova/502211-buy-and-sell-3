d4b7e5f700835dff5d51a33ef426faa77cb9b7fc
Poly Titova <poly_titova@mail.ru>
2022-03-20 11:58:59 +0300

    Добавили шаблонизатор

--- src/express/express.js
+++ src/express/express.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 
 const offersRoutes = require(`./routes/offers-routes`);
 const myRoutes = require(`./routes/my-routes`);
@@ -9,6 +10,12 @@ const mainRoutes = require(`./routes/main-routes`);
 const DEFAULT_PORT = 8080;
 
 const app = express();
+
+const PUBLIC_DIR = `public`;
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.use(`/offers`, offersRoutes);
 app.use(`/my`, myRoutes);
